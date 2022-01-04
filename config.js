7d52ee41870c9464af700bf660c8a25b5f5c1e8d
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2022-01-04 09:06:15 +0100

    Legger til bedre feilmelding om manglende env-variabler

--- config.js
+++ config.js
@@ -1,7 +1,22 @@
+function optional(key, defaultValue = undefined) {
+  return process.env[key] || defaultValue;
+}
+
+function required(key, defaultValue = undefined) {
+  const value = optional(key, defaultValue);
+  if (!value) {
+    console.error(`Missing required env: ${key}`);
+    process.exit(1);
+  }
+  return value;
+}
+
 module.exports = {
-  TEAMKATALOG_API_URL:
-    process.env.TEAMKATALOG_API_URL || "https://teamkatalog.nais.adeo.no/api",
-  TEAMKATALOG_MRH_SESSION: process.env.TEAMKATALOG_MRH_SESSION || "",
-  SLACK_SIGNING_SECRET: process.env.SLACK_SIGNING_SECRET,
-  SLACK_BOT_TOKEN: process.env.SLACK_BOT_TOKEN,
+  TEAMKATALOG_API_URL: required(
+    "TEAMKATALOG_API_URL",
+    "https://teamkatalog.nais.adeo.no/api"
+  ),
+  TEAMKATALOG_MRH_SESSION: optional("TEAMKATALOG_MRH_SESSION", ""),
+  SLACK_SIGNING_SECRET: required("SLACK_SIGNING_SECRET"),
+  SLACK_BOT_TOKEN: required("SLACK_BOT_TOKEN"),
 };
