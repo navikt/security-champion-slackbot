abca397c6ce019edf4eef9f0972ec974f5781125
Julian Ravn Thrap-Meyer <Julian.Ravn.Thrap-Meyer@nav.no>
2022-01-04 08:35:50 +0100

    Ekstrahert env-variabler til config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    TEAMKATALOG_API_URL: process.env.TEAMKATALOG_API_URL || 'https://teamkatalog.nais.adeo.no/api',
+    TEAMKATALOG_MRH_SESSION: process.env.TEAMKATALOG_MRH_SESSION || ''
+}
